f5cc5c91b3cacde911c90931be6827c4ced82533
Brody Broughton <broughton.brody07@gmail.com>
2024-09-05 15:11:21 -0600

    Added comments and updated code to acamodate tests

--- average.py
+++ average.py
@@ -1,9 +1,15 @@
 import text_utils, math
+# Imports the text utils and math to use math.floor
 
+# Total amount of words in the file using the count_words function
 totalWords = text_utils.count_words('sample.txt')
+
+# Total amount of sentences
 totalSentences = text_utils.count_sentences('sample.txt')
 
+# Averages and floors the amount of words by the sentences
 avg = math.floor(totalWords / totalSentences)
 
+# Returns it all :)
 return f'Average words per line: {avg}'
 
--- text_utils.py
+++ text_utils.py
@@ -1,11 +1,14 @@
 def count_chars(text):
 
+    # Returns the length of the text which is the amount of characters
     return len(text)
 
 def count_words(text):
     
+    # Splits the string into words and returns how many
     return len(text.split())
 
 def count_sentences(text):
     
-    return text.count('.')
+    # Counts how many sentence breakers there are and returns them
+    return text.count('.') and text.count('!') and text.count('?')
