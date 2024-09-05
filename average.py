9fc98e3660154cf50f01cfaa04d8ebc4abf177c5
Brody Broughton <broughton.brody07@gmail.com>
2024-09-05 14:56:11 -0600

    First Commit Test

--- average.py
+++ average.py
@@ -1,12 +1,9 @@
-"""
-Import the text_utils module you created and calculate the average number of
-words per line in a given text file. The text file that will be used to test
-this is the text file called "sample.txt" (located in the same directory as
-this exercise). The average number of words per line should be rounded down to
-the nearest integer.
+import text_utils, math
 
-Print the average number of words per line in the text file in the following
-format:
+totalWords = text_utils.count_words('sample.txt')
+totalSentences = text_utils.count_sentences('sample.txt')
 
-"Average words per line: [average]"
-"""
+avg = math.floor(totalWords / totalSentences)
+
+return f'Average words per line: {avg}'
+
